57f378239f78e2ef98da643b3b733e3b2ea588b7
TkachikNataly <tkachik.nataly@gmail.com>
2022-02-06 02:59:57 +0200

    tack - 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const body = document.querySelector('body');
+const butnStart = document.querySelector('[data-start]');
+const butnStop = document.querySelector('[data-stop]');
 
+butnStart.addEventListener("click", e => { 
+    timeoutId = setInterval(() => { 
+        body.style.backgroundColor = getRandomHexColor();
+        butnStart.disabled = true;
+        butnStop.disabled = false;
+        
+    },1000)
+});
+
+butnStop.addEventListener('click', e => { 
+    butnStop.disabled = true;
+    butnStart.disabled = false;
+    clearInterval(timeoutId);
+});
